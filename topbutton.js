7d50d97ce4a3611e1f8b3d82492fc67d9016f541
Samwise478 <krugsam@yahoo.com>
2018-08-07 12:14:39 -0500

    Add files via upload

---
+++ topbutton.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    $('#TopButton')
+    .hover(function () {
+        $(this).find('.fas').css("transform", "rotate(360deg)"),
+        $(this).find('.fas').css("transition-duration", "1s"),
+        $(this).find('.fas').css("transition-timing-function", "ease"),
+        $(this).find('i').toggleClass('fa-fire fa-arrow-up')   
+    },
+    function () {
+        $(this).find('.fas').css("transform", "rotate(-360deg)"),
+        $(this).find('.fas').css("transition-duration", "1s"),
+        $(this).find('.fas').css("transition-timing-function", "ease"),
+        $(this).find('i').toggleClass('fa-arrow-up fa-fire')
+    });
+
+});
+
